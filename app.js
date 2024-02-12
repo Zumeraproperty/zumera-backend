d1b1fc6011c05cb424db314220f3bd4f874e0ad9
authcornelius <corneliusedos@gmail.com>
2024-02-12 14:33:20 +0100

    testing api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Users = require('./models/newUser')
 const Subscriber = require('./models/subscribers')
-const app = express.Router();
+const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser')
 
