716a2ff160db253c2e9ffc814fc29fbfbb4e9838
authcornelius <corneliusedos@gmail.com>
2024-03-13 11:54:00 +0100

    career

--- models/subscribers.js
+++ models/subscribers.js
@@ -5,9 +5,11 @@ const Schema = mongoose.Schema;
 const subscriberSchema = new Schema({
     name: {
         type: String,
+        required: true
     },
     email: {
         type: String,
+        required: true
     }
 }, {timestamps: true});
 
